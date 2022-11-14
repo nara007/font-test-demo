278d7765da4af2c74c5221e2cb6e15da1ed5ca91
Ye Song <ye.song@iomoto.io>
2022-11-14 15:11:01 +0100

    test

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const myArgs = process.argv.slice(2);
 console.log('myArgs: ', myArgs);
 
 const file = require(__dirname + '/../env_test');
+console.log(file);
 file.development.REACT_APP_VERSION = myArgs;
 
 fs.writeFile(__dirname + '/../env_test', JSON.stringify(file), err => {
