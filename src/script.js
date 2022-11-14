fe751cc4dc6a3d8cb2586c37a19b10f7aaff08ce
Ye Song <ye.song@iomoto.io>
2022-11-14 15:17:03 +0100

    test

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ let file = JSON.parse(fs.readFileSync(__dirname + '/../env_test', 'utf8'));
 console.log(file);
 file.development.REACT_APP_VERSION = myArgs;
 
-fs.writeFile(__dirname + '/../env_test', JSON.stringify(file), err => {
+fs.writeFile(__dirname + '/../env_test', JSON.stringify(file, null, '\t'), err => {
   if (err) {
     throw err
   }
