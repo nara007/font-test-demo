8d63dae5af702500676b6597262591f3ef7eafb3
Ye Song <ye.song@iomoto.io>
2022-11-14 13:58:54 +0100

    test

--- src/script.js
+++ src/script.js
@@ -1,2 +1,11 @@
+const fs = require('fs');
 const myArgs = process.argv.slice(2);
 console.log('myArgs: ', myArgs);
+
+const data = 'This is the new content of the file.'
+fs.writeFile('../env_test', data, err => {
+  if (err) {
+    throw err
+  }
+  console.log('File is written successfully.')
+})
